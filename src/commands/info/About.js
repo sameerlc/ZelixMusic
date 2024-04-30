c54ac83eca832256c734dc1cb9972cd8a9f682ef
SAMEERLC <110273689+sameerlc@users.noreply.github.com>
2024-04-30 09:27:27 +0545

    Update About.js

--- src/commands/info/About.js
+++ src/commands/info/About.js
@@ -33,46 +33,46 @@ module.exports = class About extends Command {
   async run(client, ctx) {
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
-        .setLabel("Invite WaveMusic")
+        .setLabel("Invite ZelixMusic")
         .setStyle(ButtonStyle.Link)
         .setURL(
-          `https://discord.com/api/oauth2/authorize?client_id=875635121770889257&permissions=8&scope=bot%20applications.commands`
+          `https://discord.com/api/oauth2/authorize?client_id=875635121770889257&permissions=8&scope=bot%20applications.commands`,
         ),
       new ButtonBuilder()
         .setLabel("Support Server")
         .setStyle(ButtonStyle.Link)
-        .setURL("https://discord.gg/ns8CTk9J3e")
+        .setURL("https://discord.gg/njrn9qX"),
     );
     const embed = this.client
       .embed()
       .setAuthor({
-        name: "WaveMusic",
+        name: "ZelixMusic",
         iconURL:
           "https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png",
       })
       .setThumbnail(
-        "https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png"
+        "https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png",
       )
       .setColor(this.client.color.main)
       .addFields([
         {
-          name: "Creator",
-          value: "[Blacky#9125](https://github.com/brblacky)",
+          name: "Owner",
+          value: "[Zelix](https://guns.lol/zelix_xd)",
           inline: true,
         },
         {
-          name: "Repository",
-          value: "[Here](https://github.com/brblacky/WaveMusic)",
+          name: "Developer",
+          value: "[Zelix](https://guns.lol/zelix_xd)",
           inline: true,
         },
         {
           name: "Support",
-          value: "[Here](https://discord.gg/ns8CTk9J3e)",
+          value: "[Here](https://discord.gg/njrn9qX)",
           inline: true,
         },
         {
           name: "\u200b",
-          value: `He really wanted to make his first open source project ever for more coding experience. In this project, he was challenged to make a project with less bugs. Hope you enjoy using WaveMusic!`,
+          value: `In this project, Zelix was challenged to make a project with less bugs. Hope you enjoy using ZelixMusic!`,
           inline: true,
         },
       ]);
